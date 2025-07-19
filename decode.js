3b968fc1e833122c62e96cd76d6ee052a763917a
SvGOl <sv.golub88@gmail.com>
2025-07-18 18:03:37 -0600

    Added bootsrtap, added errors

--- decode.js
+++ decode.js
@@ -6,16 +6,19 @@ function decodeBase64() {
   const decodeInput = decodeURIComponent(document.querySelector('#decode-input').value);
   console.log(decodeInput)
   try {
+    if (decodeInput === '') {
+      throw new Error('Input is empty');
+    }
     const decoded = atob(decodeInput);
     const result = document.querySelector('.script-result');
     result.innerHTML = `
-      <h5>Result</h5>
-      <code>${decoded}</code>      
+      <h5 class="text-center">Result</h5>
+      <code class="form-control">${decoded}</code>      
       `;
     result.style.color = 'black';
   } catch (error) {
     const result = document.querySelector('.script-result');
-    result.innerHTML = `<h2>Invalid Input</h2>`;
+    result.innerHTML = `<h3>${error}</h3>`;
     result.style.color = 'red';
   }
 }
