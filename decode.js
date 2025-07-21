9fe8dfe4542d81bcd5dcb112f55be021fd5254eb
SvGOl <sv.golub88@gmail.com>
2025-07-21 15:52:41 -0600

    added two type of  decoding

--- decode.js
+++ decode.js
@@ -1,15 +1,25 @@
 let btn = document.querySelector('.decode-btn');
+let slctElem = document.querySelector('#encode_select');
+const firstInput = document.querySelector('#decode-input');
 
 btn.addEventListener('click', decodeBase64);
 
 function decodeBase64() {
-  const decodeInput = decodeURIComponent(document.querySelector('#decode-input').value);
-  console.log(decodeInput)
+  const decodeInput = decodeURIComponent(firstInput.value);
+  const slctIndex = slctElem.selectedIndex;
+
+  const decodeByIndex = () => {
+    if (slctIndex === 0) {
+      return atob(decodeInput);
+    } if (slctIndex === 1) {
+      return decodeURIComponent(decodeInput.replace(/\+/g, ' '));
+    }
+  }
   try {
     if (decodeInput === '') {
       throw new Error('Input is empty');
     }
-    const decoded = atob(decodeInput);
+    const decoded = decodeByIndex();
     const result = document.querySelector('.script-result');
     result.innerHTML = `
       <h5 class="text-center">Result</h5>
@@ -17,8 +27,8 @@ function decodeBase64() {
       `;
     result.style.color = 'black';
   } catch (error) {
-    const result = document.querySelector('.script-result');
-    result.innerHTML = `<h3>${error}</h3>`;
-    result.style.color = 'red';
+      const result = document.querySelector('.script-result');
+      result.innerHTML = `<h3>${error}</h3>`;
+      result.style.color = 'red';
   }
 }
